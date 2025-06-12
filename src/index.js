9ecbd3b50ea2185ed0b030c8b2ca57afe5a6782c
Chilet-Okeke <okekechiletaram@gmail.com>
2025-06-12 19:20:50 +0100

    update and delet push, final postman testing done

--- src/index.js
+++ src/index.js
@@ -26,10 +26,10 @@ app.post("/api/arrayList", (req, res) => {
   if (!Name || !Culture || !Food || !Drink || !Amount || !Religion) {
     return res.status(400).json({ message: "All fields are required" });
   }
-    const newItem = { Name, Culture, Food, Drink, Amount, Religion };
-    
-    arrayList.push(newItem)
-    res.status(201).json(newItem)
+  const newItem = { Name, Culture, Food, Drink, Amount, Religion };
+
+  arrayList.push(newItem);
+  res.status(201).json(newItem);
 });
 
 // Get all items in the array
@@ -49,6 +49,38 @@ app.get("/api/arrayList/:Name", (req, res) => {
   }
 });
 
-// Update
+// Update item
+app.put("/api/arrayList/:Name", (req, res) => {
+  const Name = req.params.Name;
+  const updateIndex = arrayList.findIndex((item) => item.Name === Name);
+
+  if (updateIndex === -1) {
+    return res.status(404).json({ message: "item not found" });
+  }
+
+  const { Culture, Food, Drink, Amount, Religion } = req.body;
+
+  arrayList[updateIndex] = {
+    ...arrayList[updateIndex],
+    Culture: Culture || arrayList[updateIndex].Culture,
+    Food: Food || arrayList[updateIndex].Food,
+    Drink: Drink || arrayList[updateIndex].Drink,
+    Amount: Amount || arrayList[updateIndex].Amount,
+    Religion: Religion || arrayList[updateIndex].Religion,
+  };
+  res.json(arrayList[updateIndex]);
+});
 
 //  Delete
+app.delete("/api/arrayList/:Name", (req, res) => {
+    const Name = req.params.Name
+    const deleteIndex = arrayList.findIndex(item => item.Name === Name)
+
+    if (deleteIndex === -1) {
+    return res.status(404).json({message: 'item not found'})
+        
+}   
+const deletedItem = arrayList.splice(deleteIndex, 1)
+    res.json({message: "This item has been deleted", deleted: deletedItem[0]})
+}
+)
